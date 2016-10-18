0dc9cf5116c149db4e93732c02520b8ebf734f35
Yoichi Fujimoto <wozozo@gmail.com>
2016-10-18 11:45:45 +0900

    v2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires.append('six >= 1.9.0')
 
 setup(
     name="payjp",
-    version="0.0.3",
+    version="2.0.0",
     description='PAY.JP python bindings',
     author="PAY.JP",
     author_email='support@pay.jp',
